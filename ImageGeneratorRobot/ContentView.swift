add39ce58f9621212601ebb80414cdb502bc67ce
Safar Zeynallı <115118648+safarzeynalli@users.noreply.github.com>
2022-12-15 00:50:04 +0100

    Update ContentView.swift

--- ImageGeneratorRobot/ContentView.swift
+++ ImageGeneratorRobot/ContentView.swift
@@ -5,17 +5,52 @@
 //  Created by sefer zeynalli on 14/12/2022.
 //
 
+import OpenAIKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject  var vm = viewModel()
+    @State var text = ""
+    @State var image : UIImage?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack() {
+                Spacer()
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                }
+                else {
+                    let _ = print("Type prompt to generate image!")
+                }
+                Spacer()
+                
+                TextField("Type prompt here...", text: $text)
+                    .padding()
+                Button("Generate!") {
+                    if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+                        Task {
+                            let result = await vm.generateImage(prompt: text)
+                            if result == nil {
+                                print("Failed  to get image!")
+                            }
+                            self.image = result
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Image Generator Robot")
+            .onAppear{
+                vm.setup()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
@@ -24,3 +59,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
