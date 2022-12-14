0f4fc1ceb732006b04b8c5961f70283e43ca57ee
sefer zeynalli <zeynallisefer73@gmail.com>
2022-12-14 16:56:04 +0100

    Initial Commit

---
+++ ImageGeneratorRobot/ImageGeneratorRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorRobotApp.swift
+//  ImageGeneratorRobot
+//
+//  Created by sefer zeynalli on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
